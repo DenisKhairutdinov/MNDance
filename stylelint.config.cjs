04d70484010dcf7332473fac005e457b4c56c241
Denis <jackrassul@icloud.com>
2025-07-25 22:56:19 +0500

    build: add rules

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -10,5 +10,6 @@ module.exports = {
     'order/order': ['custom-properties', 'declarations'],
     'order/properties-alphabetical-order': null,
     'scss/dollar-variable-pattern': '^[_]?[a-z]+[a-zA-Z0-9]*$',
+    "selector-class-pattern": "^[a-z]+(-[a-z0-9]+)*(__[a-z0-9]+(-[a-z0-9]+)*)?(--[a-z0-9]+(-[a-z0-9]+)*)?$",
   },
 };
