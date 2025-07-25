c5568a235d04757e8b13696c6310dff81c5ced92
Denis <jackrassul@icloud.com>
2025-07-25 23:05:50 +0500

    feat: import modules

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
+import './new-reset.css';
 import './style.scss';
+import './00_contacts/contacts.scss';
+import './00_socials/socials.scss';
+import './00_sign-up-button/sign-up-button.scss';
+import './01_header/header.scss';
