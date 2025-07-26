fd05428c0edb73de211fb718c0188409dfa064c4
Denis <jackrassul@icloud.com>
2025-07-26 21:41:50 +0500

    feat: add hero section

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './00_contacts/contacts.scss';
 import './00_socials/socials.scss';
 import './00_sign-up-button/sign-up-button.scss';
 import './01_header/header.scss';
+import './02_hero/hero.scss';
 
 //scripts
 import './full-page-scroll.js';
